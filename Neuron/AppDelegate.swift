ca5ed2a862e2469a32204bf4189be5135340257c
Scott Thorpe <sthorpe@gmail.com>
2018-01-12 14:06:55 -0700

    First commit

---
+++ Neuron/AppDelegate.swift
@@ -0,0 +1,41 @@
+//
+//  AppDelegate.swift
+//  Neuron
+//
+//  Created by Scott Thorpe on 1/4/18.
+//  Copyright © 2018 Scott Thorpe. All rights reserved.
+//
+
+import Cocoa
+import macOSThemeKit
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillFinishLaunching(_ notification: Notification) {
+        
+        /// Apply the dark theme
+        ThemeManager.darkTheme.apply()
+        
+        /// or, the light theme
+        //ThemeManager.lightTheme.apply()
+        
+        /// or, the 'system' theme, which dynamically changes to light or dark,
+        /// respecting *System Preferences > General > Appearance* setting.
+        //ThemeManager.systemTheme.apply()
+        
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,6 @@
+project 'Neuron.xcodeproj'
+
+use_frameworks!
+target 'Neuron' do
+    pod 'macOSThemeKit', '~> 1.1.0'
+end
